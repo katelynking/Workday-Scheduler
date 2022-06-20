3cf3a156688d9d44d293904451be4a3ed8f23758
Katelyn Rakonjac King <katelynking@Katelyns-MacBook-Air.local>
2022-06-20 10:31:47 -0400

    final push

--- script.js
+++ script.js
@@ -1,54 +1,32 @@
 $(document).ready(function() {
-
-var times = {
-  before: ["12am", "1am", "2am", "3am", "4am", "5am", "6am", "7am", "8am"],
-  during: ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"],
-  after: ["6pm", "7pm", "8pm", "9pm", "10pm", "11pm"]
-}; 
 var currentDay = moment().format("ddd, MMMM Do YYYY");
 $("#date").text(currentDay);
 var currentTime = moment().format("hh:mma");
 $("#time").text(currentTime);
-var currentHour;
+
   
 
 function runTimer() {
   $("#date").text(currentDay);
-  $("#time").text(currentTime);
   colorChange();
-  
-  let displayTime = setInterval(function() {
-    // currentTime = moment().format("hh:mma");
-    $("#time").text(currentTime);
-    colorChange();
-  }, 1000);
 }
 
 
 function colorChange() {
-  currentHour = moment().format("hha");
+  var currentHour = moment().hours();
 
-  if (times.before = currentHour) {
-      $(".notes").css("background-color", "#d3d3d3");
-  }
+  $(".notes").each(function() {
+    var hour = parseInt($(this).attr("id"));
 
-  if (times.after = currentHour) {
-    $(".notes").css("background-color", "#d3d3d3")
-  }
-  
-  if (times.during = currentHour) {
-    $("#" + currentHour).css("background-color", "#f378ce");
-    
-    for (let i = 0; i < times.during.indexOf(currentHour); i++) {
-      $("#" + times.during[i]).css("background-color", "#d3d3d3");
-    }
-    
-    for (let i = times.during.length - 1; i > times.during.indexOf(currentHour); i--) {
-      $("#" + times.during[i]).css("background-color", "81ca81");
-      }
-    }
-  }
-
+    if (hour > currentHour) {
+        $(this).addClass("future");
+    } else if (hour === currentHour) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("past");
+    }  
+})
+}
   runTimer();
 });
 
@@ -73,25 +51,25 @@ $(".save-btn").click(function(){
       notesInfo = $("#12pm").val();
       localStorage.setItem("12pm-text", notesInfo);
       break;
-    case "01pm-save":
-      notesInfo = $("#01pm").val();
-      localStorage.setItem("01pm-text", notesInfo);
+    case "13pm-save":
+      notesInfo = $("#13pm").val();
+      localStorage.setItem("13pm-text", notesInfo);
       break;
-    case "02pm-save":
-      notesInfo = $("#02pm").val();
-      localStorage.setItem("02pm-text", notesInfo);
+    case "14pm-save":
+      notesInfo = $("#14pm").val();
+      localStorage.setItem("14pm-text", notesInfo);
       break;
-    case "03pm-save":
-      notesInfo = $("#03pm").val();
-      localStorage.setItem("03pm-text", notesInfo);
+    case "15pm-save":
+      notesInfo = $("#15pm").val();
+      localStorage.setItem("15pm-text", notesInfo);
       break;
-    case "04pm-save":
-      notesInfo = $("#04pm").val();
-      localStorage.setItem("04pm-text", notesInfo);
+    case "16pm-save":
+      notesInfo = $("#16pm").val();
+      localStorage.setItem("16pm-text", notesInfo);
       break;
-    case "05pm-save":
-      notesInfo = $("#05pm").val();
-      localStorage.setItem("05pm-text", notesInfo);
+    case "17pm-save":
+      notesInfo = $("#17pm").val();
+      localStorage.setItem("17pm-text", notesInfo);
   }
 });
 
@@ -100,11 +78,11 @@ $(".save-btn").click(function(){
     $("#10am").text(localStorage.getItem("10am-text"));
     $("#11am").text(localStorage.getItem("11am-text"));
     $("#12pm").text(localStorage.getItem("12pm-text"));
-    $("#01pm").text(localStorage.getItem("01pm-text"));
-    $("#02pm").text(localStorage.getItem("02pm-text"));
-    $("#03pm").text(localStorage.getItem("03pm-text"));
-    $("#04pm").text(localStorage.getItem("04pm-text"));
-    $("#05pm").text(localStorage.getItem("05pm-text"));
+    $("#13pm").text(localStorage.getItem("13pm-text"));
+    $("#14pm").text(localStorage.getItem("14pm-text"));
+    $("#15pm").text(localStorage.getItem("15pm-text"));
+    $("#16pm").text(localStorage.getItem("16pm-text"));
+    $("#17pm").text(localStorage.getItem("17pm-text"));
   }
 
   renderNotes();
